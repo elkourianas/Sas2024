1709f343a42565613ed96bf49749eac589740763
elkourianas <elkoanas@gmail.com>
2024-09-25 18:34:06 +0200

    hi

---
+++ varaibl/challeng3.c
@@ -0,0 +1,20 @@
+#include <stdoi.h>
+
+int main(){
+
+float kilometres;
+
+printf("entrez kilometres");
+scanf("%f"&kilometres);
+
+yards = kilometres * 1093.61 ;
+
+
+printf("La distance en yards : %.2f yards\n", yards);"
+
+
+
+
+
+
+}
